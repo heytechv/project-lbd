454040ae688046f455facdcce8a7646a06a3d800
michalw <michalwlocka@gmail.com>
2022-08-08 11:12:26 +0200

    entity

--- src/main/java/com/lbd/projectlbd/entity/Delegation.java
+++ src/main/java/com/lbd/projectlbd/entity/Delegation.java
@@ -1,4 +1,16 @@
 package com.lbd.projectlbd.entity;
 
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Delegation")
 public class Delegation {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id") private Long id;
+    @Column(name = "start_date") private Timestamp start_date;
+//    @Column(name = "start_date") private Timestamp start_date;
+
+
 }
