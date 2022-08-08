e7dc801ae65c09280e36468f41b41d3a84b07616
wiktfic226 <wiktoriafica00@gmail.com>
2022-08-08 13:21:31 +0200

    feat: refactor master data and add sample code to use it in adding delegations

--- src/main/java/com/lbd/projectlbd/service/DelegationServiceImpl.java
+++ src/main/java/com/lbd/projectlbd/service/DelegationServiceImpl.java
@@ -1,15 +1,25 @@
 package com.lbd.projectlbd.service;
 
+import com.lbd.projectlbd.entity.Checkpoint;
 import com.lbd.projectlbd.entity.Delegation;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DelegationServiceImpl implements DelegationService {
 
     @Override public void add(Delegation delegation) {
-
-
-
+        //Wykorzystanie warunków SpEL do dodawania checkpointów do delegacji
+//        ExpressionParser parser = new SpelExpressionParser();
+//        EvaluationContext context = new StandardEvaluationContext(delegation);
+//        for(Checkpoint checkpoint : checkpointService.getAllCheckpoints()) {
+//            if(parser.parseExpression(checkpoint.getSpelExpression()).getValue(context, Boolean.class)) {
+//                ////kopiowanie tego checkpointa do listy checkpointów delegacji
+//            }
+//        }
     }
 
     @Override
--- src/main/resources/db/migration/R__master_data_checkpoints.sql
+++ src/main/resources/db/migration/R__master_data_checkpoints.sql
@@ -1,12 +1,13 @@
-CREATE TABLE masterdata_checkpoint (
-                                       id serial PRIMARY KEY,
-                                       spel_expression varchar(255) not NULL,
-                                       description varchar(255) not NULL
+CREATE TABLE masterdata_checkpoint
+(
+    id              serial PRIMARY KEY,
+    spel_expression varchar(255) not NULL,
+    description     varchar(255) not NULL
 );
- 
-INSERT INTO masterdata_checkpoint(spel_expression, description) VALUES
-                                                                    ('country != ''PL''', 'Is the flight ticked purchased?'),
-                                                                    ('country != ''PL''', 'Is covid test done?'),
-                                                                    ('T(java.time.temporal.ChronoUnit).DAYS.between(date_begin,date_end) >= 2', 'Is the hotel booked?'),
-                                                                    ('true', 'Is diet paid off?'),
-                                                                    ('true', 'Is meeting schedule definied?');
+
+INSERT INTO masterdata_checkpoint(spel_expression, description)
+VALUES ('country != ''PL''', 'Is the flight ticked purchased?'),
+       ('country != ''PL''', 'Is covid test done?'),
+       ('T(java.time.temporal.ChronoUnit).DAYS.between(date_begin,date_end) >= 2', 'Is the hotel booked?'),
+       ('true', 'Is diet paid off?'),
+       ('true', 'Is meeting schedule definied?');
