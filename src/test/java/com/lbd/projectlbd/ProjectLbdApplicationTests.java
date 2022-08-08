4fbf35f5f60945ffe3cdc19a9eee1f6f11704feb
michalw <michalwlocka@gmail.com>
2022-08-08 10:51:56 +0200

    Initial commit

---
+++ src/test/java/com/lbd/projectlbd/ProjectLbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbd.projectlbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
